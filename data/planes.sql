ec1a90d00b648161a56fd90e3a3ab3854beca943
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-10-21 21:08:47 -0300

    Fix fields on plane

--- data/planes.sql
+++ data/planes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE planes (
   name VARCHAR(68),
-  iso_code VARCHAR(3),
-  dafif_code VARCHAR(4)
+  iata_code VARCHAR(3),
+  icao_code VARCHAR(4)
 );
 
-COPY planes(name, iso_code, dafif_code)
+COPY planes(name, iata_code, icao_code)
 FROM '/planes.csv' DELIMITER ',' CSV;
