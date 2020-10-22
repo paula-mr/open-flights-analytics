35e2668a8c87c9ed7f041e5ddace1773fe1823ae
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-10-22 20:07:53 -0300

    Comments insert command in routes so we ignore missing FKS

--- data/routes.sql
+++ data/routes.sql
@@ -1,4 +1,5 @@
 CREATE TABLE routes (
+  route_id SERIAL PRIMARY KEY,
   airline VARCHAR(3),
   airline_id bigint,
   source_airport VARCHAR(72),
@@ -7,8 +8,11 @@ CREATE TABLE routes (
   destination_airport_id bigint,
   codeshare VARCHAR(1),
   stops bigint,
-  equipment VARCHAR(100)
+  equipment VARCHAR(100),
+  FOREIGN KEY (airline_id) REFERENCES airlines(airline_id),
+  FOREIGN KEY (source_airport_id) REFERENCES airports(airport_id),
+  FOREIGN KEY (destination_airport_id) REFERENCES airports(airport_id)
 );
 
-COPY routes(airline, airline_id, source_airport, source_airport_id, destination_airport, destination_airport_id, codeshare, stops, equipment)
-FROM '/routes.csv' DELIMITER ',' CSV;
+/*COPY routes(airline, airline_id, source_airport, source_airport_id, destination_airport, destination_airport_id, codeshare, stops, equipment)
+FROM '/routes-treated.csv' DELIMITER ',' CSV;*/
